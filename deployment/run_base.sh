98a46ac113d0961704f4bb2e4542f2283ec7052a
David Valentine <david.valentine@gmail.com>
2022-06-29 09:31:53 -0700

    add base-machine-compose.yaml for portainer/traefik

---
+++ deployment/run_base.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+helpFunction()
+{
+   echo "setup a base portainer and traefik command line"
+   echo "Usage: $0 -e envfile  -r  -d "
+   echo -e "\t-e envfile to use"
+   echo -e "\t-u DO NOT RUN detached"
+      echo -e "\tMAC: You may have to add /tmp/gleaner to the file sharing preferences in the docker app"
+   exit 1 # Exit script after printing help
+}
+detached=true
+
+while getopts "e:rd" opt
+do
+   case "$opt" in
+      e ) envfile="$OPTARG" ;;
+      u ) detached=false ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+if [ ! $envfile ]
+  then
+     envfile=".env"
+fi
+
+## need to docker (network|volume) ls | grep (traefik_proxy|traefik_proxy) before these calll
+## or an error will be thrown
+docker network create traefik_proxy
+docker volume create traefik_proxy
+
+if [ $detached ]
+  then
+    docker compose -p base --env-file $envfile  -f base-machine-compose.yaml  up -d
+  else
+    docker compose -p base --env-file $envfile  -f base-machine-compose.yaml  up
+fi
